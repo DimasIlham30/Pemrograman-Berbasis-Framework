8d2ca5ac9f4d7c886cc90b8fe44c3bb703c32b63
DimasIlham30 <70867817+DimasIlham30@users.noreply.github.com>
2022-04-03 21:27:35 +0700

    Delete Pertemuan 6/react-redux directory

--- Pertemuan 6/react-redux/src/reducers/MainReducers.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import todos from './TodoReducers';
-import visibilityFilter from './FilterReducers';
-
-export default combineReducers({
- todos,
- visibilityFilter
-})
