24fe360555dc1aaff207e4869cdf3c397c51e1b5
DimasIlham30 <midxinnn@gmail.com>
2022-04-28 11:01:00 +0700

    Upload praktikum modul 10

---
+++ Pertemuan 10/firebase-login/src/firebase.config.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyC2AzPXlxCN-ZD1OBIT9HKmMqcflFwZeAM",
+    authDomain: "dimas-firebase.firebaseapp.com",
+    projectId: "dimas-firebase",
+    storageBucket: "dimas-firebase.appspot.com",
+    messagingSenderId: "634418786526",
+    appId: "1:634418786526:web:6c6f6c4c42b0c8b652124f",
+    measurementId: "G-CY19SKPMZH"
+}; 
